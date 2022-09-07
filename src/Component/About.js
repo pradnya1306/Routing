5fddb4be83e096302035c1f91a42bb7f1e97ee29
Pradnya Pokharkar <pardnya20@navgurukul.org>
2022-09-07 20:06:25 +0530

    intialise project

---
+++ src/Component/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function About(){
+    return(
+      <>
+    <h1>about page</h1>
+    <h2>it all about 'About' page</h2>
+    </>
+    )
+  }
+  export default About;
---
+++ src/Component/Navbar.js
@@ -0,0 +1,16 @@
+import React from "react"
+import {Link} from 'react-router-dom';
+
+function  Navbar(){
+    return(
+        <nav>
+            <Link to="/">Home </Link>
+            <Link to="/about"> About</Link>
+            
+           
+            
+        </nav>
+
+    )
+}
+export default Navbar;
